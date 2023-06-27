f46996f4d98a4943abdbff7615d39f31dc06f585
Marina Kuzmina <maridev40@mail.ru>
2023-06-28 00:07:32 +0300

    Lesson 4. Initial commit

---
+++ lesson4/Main.java
@@ -0,0 +1,20 @@
+package lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        
+        tree.add(6);
+        tree.add(3);
+        tree.add(2);
+        tree.add(4);
+        tree.add(5);
+        tree.remove(6);
+        tree.remove(5);
+        //tree.remove(4);
+
+        tree.print();
+    
+    }
+}
